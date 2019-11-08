52d0c384270db6b4a1814d333ef40e5f16b691c5
yuki23329626 <michael7105@gmail.com>
2019-11-08 11:59:22 +0800

    add comments in stop.sh

--- stop.sh
+++ stop.sh
@@ -7,11 +7,13 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 echo "\nHello there, it a shell script for deleting all the settings\n"
 
+# 停止所有服務
 systemctl stop apache2.service
 systemctl stop isc-dhcp-server.service
 systemctl stop hostapd.service 
 sudo systemctl restart networking
 
+# 清除所有防火牆資料
 iptables -Z
 iptables -F
 iptables -X
